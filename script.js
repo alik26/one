ca362dfaa52f1e0d75c363df7c0243234579922f
Ali <auezovali925@gmail.com>
2020-03-08 20:36:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let arr = ["a","b","c"];
+
+let ans = true;
+let abs = true;
+let asd = false
+console.log(ans||abs||asd);
